8a7e8376d1c6ba33e9934e01dffd36f0d9e4d157
Lucas Augusto <luckaugusto@gmail.com>
2016-09-17 23:00:52 -0300

    Projeto Configurado VRaptor
    VRaptor 4
    Java 8
    Tomcat 8
    Maven for Dependencies
    Hibernate

---
+++ src/main/java/com/healthsurgery/controller/FirstController.java
@@ -0,0 +1,20 @@
+package com.healthsurgery.controller;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Path;
+
+@Controller
+public class FirstController {
+
+	@Path("/first/hello")
+	public void hello(){
+		
+	}
+
+	@Get("/")
+    public String index() {
+        System.out.println("primeiro projeto com VRaptor 4!");
+        return "Primeiro Teste, Olá mundo!";
+    }
+}
